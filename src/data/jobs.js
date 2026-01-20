368205e23c0fcfeff4e89b0053514da5f83bb2ce
TheDutchman68 <nd527589@gmail.com>
2026-01-20 16:32:23 +0200

    add mock jobs data

---
+++ src/data/jobs.js
@@ -0,0 +1,16 @@
+export const jobs = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        company: "Google",
+        location: "Remote",
+        description: "React developer needed"
+    },
+    {
+        id: 2,
+        title: "React Junior",
+        company: "Microsoft",
+        location: "Belgium",
+        description: "Entry level position"
+    }
+];
