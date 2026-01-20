5f981a2e86ba947778a53a13e4074fc7de03ec77
TheDutchman68 <nd527589@gmail.com>
2026-01-20 17:27:11 +0200

    Replace mock data iwth fake API using json-server

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,8 +1,33 @@
-import { jobs } from "../data/jobs";
+import {useEffect, useState } from "react";
 import JobCard from "./JobCard";
 
 
 function JobList(){
+
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error,setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:5001/jobs")
+        .then(res => {
+            if (!res.ok) throw new Error("Failed to fetch jobs");
+            return res.json();
+        })
+        .then(data => {
+            setJobs(data);
+            setLoading(false);
+        })
+        .catch(err => {
+            setError(err.message);
+            setLoading(false);
+        });
+
+    },[]);
+
+    if (loading) return <p>Loading jobs...</p>
+    if (error) return <p>{error}</p>
+
     return(
         <div>
         {jobs.map(job => (
--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
-import { jobs } from "../data/jobs";
+import { useEffect, useState } from "react";
 
 function JobDetails(){
     const { id } = useParams();
-    const job = jobs.find(j => j.id === Number(id));
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() =>{
+        fetch(`http://localhost:5001/jobs/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            setJob(data);
+            setLoading(false);
+        })
+    },[id]);
+
+    if (loading) return <p>Loading...</p>;
     if (!job) return <p>Job not found</p>;
 
     return(      
