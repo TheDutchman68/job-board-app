50b4f106d21d2f5e562343ec034387015c64dfc9
TheDutchman68 <nd527589@gmail.com>
2026-01-20 17:02:09 +0200

    add navigation to job details

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function JobCard({job}){
     return(
         <div>
             <h3>{job.title}</h3>
             <p>{job.company}</p>
-            <p>{job.location}</p>
+            <Link to={`/jobs/${job.id}`}>View Details</Link>
         </div>
     );
 }
